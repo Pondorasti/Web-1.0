201ac6e2354b4dc2b1e695945b4564037a085468
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2020-09-22 11:30:57 -0700

    Tip Calculator - Basic Functionality & Challenge 1

---
+++ Class Work/Tip Calculator/script.js
@@ -0,0 +1,24 @@
+const billInput = document.querySelector("#bill")
+const tipInput = document.querySelector("#tip")
+const peopleInput = document.querySelector("#people")
+
+const displayTip = document.querySelector("#display-tip")
+const displayTotal = document.querySelector("#display-total")
+
+function calculateTip() {
+    const billValue = parseInt(billInput.value)
+    const tipValue = parseInt(tipInput.value)
+    const peopleValue = parseInt(peopleInput.value)
+
+    const tipAmount = billValue * tipValue / 100 / peopleValue
+    const total = (billValue + tipAmount) / peopleValue
+
+    displayTip.innerHTML = `Tip: ${tipAmount.toFixed(2)}`
+    displayTotal.innerHTML = `Total: ${total.toFixed(2)}`
+}
+
+billInput.addEventListener('input', calculateTip)
+tipInput.addEventListener('input', calculateTip)
+peopleInput.addEventListener('input', calculateTip)
+
+calculateTip()
